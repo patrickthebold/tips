793eb6da4f0f146edc04e40ce7aa551f7183937d
Patrick Curran <patrickthebold@gmail.com>
2017-03-21 21:13:47 -0400

    Update scripts.

--- cleandb.sh
+++ cleandb.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
+if [ -d "$TIPS_DB_DIR" ]; then
+	if pg_ctl status  -D "$TIPS_DB_DIR"; then
+		. stopdb.sh
+	fi
 
-rm -rf "$TIPS_DB_DIR"
+	rm -rf "$TIPS_DB_DIR"
+else
+	echo "$TIPS_DB_DIR does not exist. Nothing to do"
+fi
